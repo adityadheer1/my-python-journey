b28670355d08c9e681f973b303d62f3832e05693
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-08-14 13:10:40 +0530

    Add files via upload

---
+++ loops_1.py
@@ -0,0 +1,9 @@
+#Count positive numbers in an array
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+positive_number_count = 0
+
+for num in numbers:
+    if num > 0:
+        positive_number_count = positive_number_count + 1
+
+print(positive_number_count)
